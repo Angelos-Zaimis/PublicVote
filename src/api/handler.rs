ff3d6eb00b18f7cbccffe943beb2e3e86bbf034c
AngelosZa <aggeloszaimiis@gmail.com>
2025-03-06 23:42:20 +0100

    feat: webserver, handlers and services

---
+++ src/api/handler.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+use axum::extract::State;
+use axum::Json;
+use crate::api::dtos::VoteRequestDTO;
+use validator::{Validate};
+use crate::api::voting_service::VotingService;
+
+pub async fn handle_create_vote(
+    State(service): State<Arc<VotingService>>,
+    Json(payload): Json<VoteRequestDTO>,
+) -> Json<String> {
+
+    if service.voter_has_voted(payload.voter_id.clone()) {
+        return Json("You have already voted.".to_string());
+    }
+
+    Json("You voted successfully!".to_string())
+}
+
+fn validate_payload(payload: &VoteRequestDTO) {
+    payload.validate().expect("VoteRequestDTO failed");
+
+}
